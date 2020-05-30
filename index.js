a625c1734da94225271e030919668a54f8e0f297
Hari <hariprasadk125@gmail.com>
2020-05-30 19:50:46 +0530

    functions are created and basic functioning is ready

---
+++ index.js
@@ -0,0 +1,26 @@
+
+var userPoints = 0;
+var comPoints = 0;
+var count = 0;
+
+function game(userinput){
+    var com = Math.floor(Math.random()*6)+1;
+    count+=1
+   if(count<=12){
+    if(userinput==com){
+        document.getElementById("res").innerHTML="OUT"
+    }
+    else{
+
+        userPoints+=parseInt(userinput);
+        comPoints +=parseInt(com);
+        document.getElementById("score").innerHTML=userPoints +" - "+ comPoints
+    }
+   }
+  else if(userPoints>comPoints){
+    document.getElementById("res").innerHTML="You won the game"
+  }
+  else if(userPoints<comPoints){
+    document.getElementById("res").innerHTML="Computer won the game"
+  }
+}
