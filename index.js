7b953e77e3dbf8e743a3d38da9c71cbf463111e0
Hari <hariprasadk125@gmail.com>
2020-05-31 10:01:23 +0530

    landing page added

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 var userPoints = 0;
 var comPoints = 0;
 var count = 0;
+var player = "";
 
-function game(userinput){
+function bat(userinput){
     var com = Math.floor(Math.random()*6)+1;
     count+=1
    if(count<=12){
@@ -11,7 +12,7 @@ function game(userinput){
         document.getElementById("res").innerHTML="OUT"
     }
     else{
-        document.getElementById("res").innerHTML=""
+        document.getElementById("res").innerHTML=" "
         userPoints+=parseInt(userinput);
         comPoints +=parseInt(com);
         document.getElementById("score").innerHTML=userPoints +" - "+ comPoints
@@ -23,4 +24,22 @@ function game(userinput){
   else if(userPoints<comPoints){
     document.getElementById("res").innerHTML="Computer won the game"
   }
+}
+
+function validate(value){
+  console.log(value)
+    if (value.length<4){
+      document.getElementById("err").innerHTML="Please enter atleast 4 characters";
+      document.getElementById("btn-st").disabled=true;
+    }
+    else{
+      player = value;
+      document.getElementById("err").innerHTML="";
+      document.getElementById("btn-st").disabled=false;
+    }
+}
+
+function start(){
+  document.getElementById("start").innerHTML=""
+  document.getElementById("bat").hidden=false;
 }
